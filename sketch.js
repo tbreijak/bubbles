3787c26fdce07452ca8d084413efd259325474e2
tbreijak <ab6131@wayne.edu>
2016-02-16 07:36:56 -0500

    coded
    made 500 bubbles

--- sketch.js
+++ sketch.js
@@ -1,19 +1,32 @@
 var bubbles = [];
 
+var addBubbles = function (numberOfBubbles) {
+  for (var i = 0; i < numberOfBubbles; ++i) {
+    bubbles[i] = new Bubble(random(0, width), random(0, height));
+  }
+};
+
+var clickMouse = function () {
+  //bubbles.push(new Bubble(mouseX, mouseY));
+  bubbles[bubbles.length] = new Bubble(mouseX, mouseY);
+  //addBubbles(500);
+};
+
 setup = function () {
   createCanvas(600, 600);
+  addBubbles(500);
   // your code goes here
+
 };
 
 draw = function () {
   background(0);
   // your code goes here
-  if (mouseIsPressed) bubbles.push(new Bubble(mouseX, mouseY));
-
-  for (var i = 0; i < bubbles.length; ++i) {
-    bubbles[i].update();
-    bubbles[i].display();
-  };
+  if (mouseIsPressed) clickMouse();
+  for (var index = 0; index < bubbles.length; index = index + 1) {
+    bubbles[index].update();
+    bubbles[index].display();
+  }
 };
 
 // we are in the 500-bubbles branch
