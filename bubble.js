8b30a107d9fee7e40575f4c21e38cf940c9e878a
tbreijak <ab6131@wayne.edu>
2016-02-18 17:04:46 -0500

    made continuous bubbles
    flowing bubbles accomplished

--- bubble.js
+++ bubble.js
@@ -1,6 +1,7 @@
 var Bubble = function (x, y) {
   this.x = x;
   this.y = y;
+
 };
 
 Bubble.prototype = {
@@ -10,13 +11,14 @@ Bubble.prototype = {
   update: function() {
     this.x += random(-1, 1);
     this.y += random(0, -2);
-    if (this.y - this.radius <= 0) this.y = this.radius + random(0, 2);
+    if (this.y < this.radius * -1) this.y = this.radius + height;
   },
 
   display: function () {
     noStroke();
     fill(102, 217, 255, 100);
     ellipse(this.x, this.y, this.radius * 2, this.radius * 2);
+
   }
 
 };
