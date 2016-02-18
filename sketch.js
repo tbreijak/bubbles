39f65f63f0a8f7b5ce6245407331f1e3bef97084
tbreijak <ab6131@wayne.edu>
2016-02-18 17:37:28 -0500

    changed from for loop to forEach
    changed from for loop to forEach

--- sketch.js
+++ sketch.js
@@ -1,5 +1,8 @@
 var bubbles = [];
-
+var updateAndDisplay = function(bubbles) { // this assigns our function to a variable
+    bubbles.update();
+    bubbles.display();
+};
 setup = function () {
   createCanvas(600, 600);
   // your code goes here
@@ -7,11 +10,10 @@ setup = function () {
 
 draw = function () {
   background(0);
-  // your code goes here
+  bubbles.forEach(updateAndDisplay); // your code goes here
   if (mouseIsPressed) bubbles.push(new Bubble(mouseX, mouseY));
 
-  for (var i = 0; i < bubbles.length; ++i) {
-    bubbles[i].update();
-    bubbles[i].display();
+  // for (var i = 0; i < bubbles.length; ++i) {
+  //   bubbles[i].update();
+  //   bubbles[i].display();
   };
-};
