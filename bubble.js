72cf9aed8ff9934dddce90cf832c106860084254
tbreijak <ab6131@wayne.edu>
2016-02-18 16:32:47 -0500

    trying out some code
    copied code

--- bubble.js
+++ bubble.js
@@ -1,22 +1,45 @@
-var Bubble = function (x, y) {
+var Bubble = function(x, y) {
   this.x = x;
   this.y = y;
+  this.speed = {x: 1, y: 1}; // because our speed is represented by an object, we *must* put it in the constructor
 };
 
 Bubble.prototype = {
 
-  radius: 5,
+  radius: 7,
 
   update: function() {
     this.x += random(-1, 1);
     this.y += random(0, -2);
-    if (this.y - this.radius <= 0) this.y = this.radius + random(0, 2);
+    if(this.y - this.radius <= 0)  this.y = this.radius + random(0, 2);
   },
 
-  display: function () {
+  display: function() {
     noStroke();
     fill(102, 217, 255, 100);
     ellipse(this.x, this.y, this.radius * 2, this.radius * 2);
-  }
+  },
 
+  //bounce: function() {
+    //this.bounceX();
+    //this.bounceY();
+  //},
+
+  //bounceX: function() {
+    //if (this.x + this.radius > width) {
+      //this.speed.x *= -1;
+  //  }
+    //if (this.x - this.radius < 0) {
+      //this.speed.x *= -1;
+//    }
+  //},
+
+  //bounceY: function() {
+  //  if (this.y + this.radius > height) {
+    //  this.speed.y *= -1;
+    //}
+    //if (this.y - this.radius < 0) {
+      //this.speed.y *= -1;
+    //}
+  //}
 };
