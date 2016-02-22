e32894884b4669f004c2f50f2606ceb7bb6341e0
tbreijak <ab6131@wayne.edu>
2016-02-22 18:40:51 -0500

    making snow
    making snow

--- bubble.js
+++ bubble.js
@@ -5,18 +5,18 @@ var Bubble = function (x, y) {
 
 Bubble.prototype = {
 
-  radius: 5,
+  radius: 3,
 
   update: function() {
     this.x += random(-1, 1);
-    this.y += random(0, -2);
+    this.y -= (1, -2);
     if (this.y - this.radius <= 0) this.y = this.radius + random(0, 2);
   },
 
   display: function () {
     noStroke();
-    fill(102, 217, 255, 100);
-    ellipse(this.x, this.y, this.radius * 2, this.radius * 2);
+    fill(255);
+    ellipse(this.x, this.y, this.radius, this.radius);
   }
 
 };
